97c5b452c764fa9931baca3d17239cfb4e90c2c3
DESKTOP-KGQR7QK\Vinicius <vinicius.s.dornelas@gmail.com>
2019-05-15 14:10:57 -0300

    Login do w9boxrh agora é por cpf e email

---
+++ config/autoload/local.php
@@ -0,0 +1,13 @@
+<?php
+
+//DEFINE CONSTANTS 
+defined('SITE_URL')
+    || define('SITE_URL', 'http://www.sigaalliar.com.br');
+
+return array(
+    'db'   => array(
+        'dsn' => 'mysql:dbname=bd_sigaavaliacoes;host=localhost',
+        'username'      => 'root',
+        'password'      => ''
+    ),
+);
